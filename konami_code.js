e8665ccb434dfb3750e3b952c9686e8f86be438e
curlywallst <nnoyes@comcast.net>
2017-12-12 17:32:11 -0500

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,45 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
-  // Write your JavaScript code inside the init() function
+  let index = 0
+  const input = document.querySelector('body')
+  input.addEventListener('keydown', function(e) {
+    let key = parseInt(e.detail || e.which);
+    console.log(e)
+    console.log(e.detail, e.which, e.location)
 
+    if (key === code[index]) {
+      index++;
+
+      if (index === code.length) {
+        alert("Hurray!");
+
+        index = 0;
+      }
+    } else {
+      if (key !== 16) {
+        index = 0;
+      }
+    }
+  })
 }
+
+// function parseBrackets(string) {
+//   let openersFound = []
+//   let openers = ['{', '(', '[']
+//   let closers = { '}': '{', ')': '(', ']': '[' }
+//   for (let i=0; i<string.length; i++) {
+//     if (openers.includes(string[i])) {
+//       openersFound.push(string[i])
+//     } else if (Object.keys(closers).includes(string[i])) {
+//       if (closers[string[i]] !== openersFound.pop()) {
+//         return 'not balanced'
+//       }
+//     }
+//   }
+//   if (openersFound.length === 0) {
+//       return 'balanced'
+//   } else {
+//     return 'not balanced'
+//   }
+// }
